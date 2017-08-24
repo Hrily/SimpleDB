f78d0316a0363c0286385cd8b64f8e9ad803bbc7
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2017-08-24 17:48:20 +0530

    Project 1 Testing Done

---
+++ src/java/simpledb/Project1Test.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simpledb;
+
+import java.io.*;
+
+/**
+ *
+ * @author hrishi
+ */
+public class Project1Test {
+    
+    public static String FILEPATH = "/home/hrishi/git/SimpleDB/test/simpledb/sample_data/data1.dat";
+    
+    public static void main(String[] argv) {
+
+        // construct a 3-column table schema
+        Type types[] = new Type[]{ Type.INT_TYPE, Type.INT_TYPE, Type.INT_TYPE };
+        String names[] = new String[]{ "field0", "field1", "field2", "field4" };
+        TupleDesc descriptor = new TupleDesc(types, names);
+        
+        // create the table, associate it with some_data_file.dat
+        // and tell the catalog about the schema of this table.
+        HeapFile table1 = new HeapFile(new File(FILEPATH), descriptor);
+        Database.getCatalog().addTable(table1, "test1");
+        
+        // construct the query: we use a simple SeqScan, which spoonfeeds
+        // tuples via its iterator.
+        TransactionId tid = new TransactionId();
+        SeqScan f = new SeqScan(tid, table1.getId());
+        
+        try {
+            // and run it
+            f.open();
+            while (f.hasNext()) {
+                Tuple tup = f.next();
+                System.out.println(tup);
+            }
+            f.close();
+            Database.getBufferPool().transactionComplete(tid);
+        } catch (Exception e) {
+            System.out.println ("Exception : " + e);
+        }
+    }
+    
+}
